cea50ff94a7693a327da399c3b1fc4e694036c4a
Asher <asher@h.local>
2022-01-31 12:49:07 -0500

    Commit 0 of 1

--- commit.py
+++ commit.py
@@ -1,4 +1,5 @@
 import os
+
 ip = int(input("How many times do you want to commit? \n"))
 autoPush = input("Auto git push when commited? (y/n) \n")
 
@@ -10,5 +11,7 @@ print("Commited " + str(ip) + " times")
 
 if autoPush == "y":
 	os.system('git push')
- 
+	
 # git commit --allow-empty -m "New Commit at: $(date)"
+
+# TROLL
